9b1428ee49cd6bb6e009527aa2d51bcf662f3c67
Fanbo Xiang <fxiang@eng.ucsd.edu>
2023-11-04 01:15:11 -0700

    new wheel model

--- sapien_google_robot/__init__.py
+++ sapien_google_robot/__init__.py
@@ -24,16 +24,16 @@ class EverydayRobot(sapien.Widget):
         loader.set_link_min_patch_radius("link_finger_right", 0.05)
         loader.set_link_min_patch_radius("link_finger_tip_right", 0.05)
 
-        loader.set_link_material("link_wheel_left", 1.0, 1.0, 0.0)
-        loader.set_link_material("link_wheel_right", 1.0, 1.0, 0.0)
-        loader.set_link_material("link_rear_wheel_left", 1.0, 1.0, 0.0)
-        loader.set_link_material("link_rear_wheel_right", 1.0, 1.0, 0.0)
+        loader.set_link_material("link_wheel_left", 0.0, 0.0, 0.0)
+        loader.set_link_material("link_wheel_right", 0.0, 0.0, 0.0)
+        loader.set_link_material("link_rear_wheel_left", 0.0, 0.0, 0.0)
+        loader.set_link_material("link_rear_wheel_right", 0.0, 0.0, 0.0)
 
         loader.fix_root_link = False
-        # loader.fix_root_link = True
 
         path = os.path.join(
-            os.path.dirname(__file__), "./googlerobot_description/meta_sim.urdf"
+            os.path.dirname(__file__),
+            "./googlerobot_description/meta_sim_fix_wheel.urdf",
         )
         self.robot = loader.load(path)
         pose = self.robot.pose
@@ -47,14 +47,6 @@ class EverydayRobot(sapien.Widget):
         self.gripper_joints = [
             self.robot.find_joint_by_name(j)
             for j in ["joint_finger_left", "joint_finger_right"]
-        ]
-
-        self.wheel_joints = [
-            self.robot.find_joint_by_name(j)
-            for j in [
-                "joint_wheel_left",
-                "joint_wheel_right",
-            ]
         ]
 
         self.arm_joints = [
@@ -79,7 +71,18 @@ class EverydayRobot(sapien.Widget):
 
         self.set_arm_pd([2000] * 7, [200] * 7, [50] * 7)
         self.set_gripper_pd(1e4, 1e2, 0.3)
-        self.set_wheel_damping(1e5, 1e3)
+
+        c = sapien.physx.PhysxRigidDynamicComponent()
+        c.set_kinematic(True)
+        self.drive_dummy = sapien.Entity().add_component(c).add_to_scene(scene)
+
+        self.base_drive = scene.create_drive(
+            self.drive_dummy,
+            sapien.Pose(q=[0.7071068, 0, -0.7071068, 0]),
+            self.robot.root,
+            sapien.Pose(q=[0.7071068, 0, -0.7071068, 0]),
+        )
+        self.set_base_drive_damping(1e4, 1e4)
 
     def _setup_passive_drives(self):
         for n in ["joint_finger_tip_left", "joint_finger_tip_right"]:
@@ -115,14 +118,15 @@ class EverydayRobot(sapien.Widget):
         for j in self.gripper_joints:
             j.set_drive_target(target)
 
-    def set_wheel_damping(self, damping, limit):
-        for j in self.wheel_joints:
-            j.set_drive_property(0, damping, limit, "acceleration")
+    def set_base_drive_damping(self, linear, angular):
+        self.base_drive.set_drive_property_z(0, linear, mode="acceleration")
+        self.base_drive.set_drive_property_twist(0, angular, mode="acceleration")
 
-    def set_wheel_target_velocity(self, vel):
-        assert len(vel) == 2
-        self.wheel_joints[0].set_drive_velocity_target(vel[0])
-        self.wheel_joints[1].set_drive_velocity_target(vel[1])
+    def step(self):
+        self.drive_dummy.set_pose(self.robot.root.pose)
+
+    def set_base_drive(self, linear, angular):
+        self.base_drive.set_drive_velocity_target([0, 0, -linear], [-angular, 0, 0])
 
     def unload(self, scene: sapien.Scene):
         scene.remove_articulation(self.robot)
@@ -134,12 +138,13 @@ if __name__ == "__main__":
     robot = EverydayRobot()
     scene.load_widget(robot)
 
-    robot.set_wheel_target_velocity([10, 10])
+    robot.set_base_drive(1, 0.5)
 
     scene.set_timestep(1 / 120)
 
     viewer = scene.create_viewer()
     while not viewer.closed:
+        robot.step()
         scene.step()
         scene.update_render()
         viewer.render()
